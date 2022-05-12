7844401c5cb8b5e32f8f10263fb73db946f94af4
ivanu74 <49431820+ivanu74@users.noreply.github.com>
2022-05-12 14:45:31 +0300

    BE-4632: add prepared error from response (#2)
    add prepared error from response
    
    Co-authored-by: Ivan Urazov <ivan.urazov@humans.net>

---
+++ error.go
@@ -0,0 +1,49 @@
+package es
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+)
+
+type Error struct {
+	StatusCode int
+	Type       string
+	Reason     string
+}
+
+func (e Error) Error() string {
+	return fmt.Sprintf("Error: [%d] %s: %s",
+		e.StatusCode,
+		e.Type,
+		e.Reason,
+	)
+}
+
+func ExtractError(res *esapi.Response) error {
+	if !res.IsError() {
+		return nil
+	}
+
+	var raw map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&raw); err != nil {
+		return fmt.Errorf("Failure to parse response body: %w", err)
+	}
+
+	typ, ok := raw["error"].(map[string]interface{})["type"].(string)
+	if !ok {
+		return fmt.Errorf("Failure to parse type to string")
+	}
+
+	reason, ok := raw["error"].(map[string]interface{})["reason"].(string)
+	if !ok {
+		return fmt.Errorf("Failure to parse reason to string")
+	}
+
+	return Error{
+		StatusCode: res.StatusCode,
+		Type:       typ,
+		Reason:     reason,
+	}
+}
